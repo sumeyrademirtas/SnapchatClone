21e1028e860e9fbced96e9dc54aac92e42125154
sumeyrademirtas <smyrdmrts@gmail.com>
2024-06-17 14:30:57 +0300

    feat: enable image upload functionality from photo library
    # enabled user interaction on uploadImageView
    # added UITapGestureRecognizer to uploadImageView
    # implemented choosePicture to present UIImagePickerController
    # handled image selection with imagePickerController(_:didFinishPickingMediaWithInfo:)

--- SnapchatClone/ViewControllers/UploadVC.swift
+++ SnapchatClone/ViewControllers/UploadVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class UploadVC: UIViewController {
+class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var uploadImageView: UIImageView!
     
@@ -15,6 +15,23 @@ class UploadVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        uploadImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(choosePicture))
+        uploadImageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func choosePicture() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        uploadImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func uploadClicked(_ sender: Any) {
